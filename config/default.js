f7315c49fd1ed6fbfb7a4ee462b7d18cc48b9abf
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-07-28 11:48:21 +0900

    server.js: updating server

--- config/default.js
+++
@@ -1,3 +0,0 @@
-{
-    "mongoURI": "mongodb+srv://Admin:y6oFpEteia26Wa7o@animal-crossing-i1pnl.mongodb.net/test?retryWrites=true&w=majority"
-}
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
-
-connectDB();
-
-app.use(express.json({extended: false}));
-
+app.get("/", (req, res) => res.send("API's running nicely boss"));
+app.use(express.json({ extended: false }));
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> console.log(`Server loaded on ${PORT}`));
+app.listen(PORT, () => console.log(`Server loaded on ${PORT}`));
